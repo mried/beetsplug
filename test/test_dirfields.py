595f6d2870e318aa160323cb03f04beeae6fab91
Malte Ried <mried@users.noreply.github.com>
2015-01-20 19:59:49 +0100

    Cleaned up the license texts

--- test/test_dirfields.py
+++ test/test_dirfields.py
@@ -11,6 +11,7 @@
 #
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
+
 import os
 from beets import config
 import beetsplug
